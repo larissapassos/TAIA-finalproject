64ce7f44a3be3a6d49042a23dc6630d6695ae98c
Guilherme Peixoto <gpp@cin.ufpe.br>
2016-06-24 17:40:10 -0300

    code modularization

--- classifier.py
+++ classifier.py
@@ -4,10 +4,10 @@ from sklearn.feature_extraction.text import TfidfVectorizer as Tfidf
 from ast import literal_eval as eval
 from nltk.corpus import stopwords
 from twokenize import tokenize as tweet_tokenizer
-import Processor
+import Processor, sys
 import numpy as np
 
-def main(C, gamma):
+def process_corpus():
     lines = [eval(line.strip()) for line in open("tweetlabels.txt", "r")]
     stop = stopwords.words("english")
     stop.remove("no")
@@ -16,9 +16,12 @@ def main(C, gamma):
     pr = Processor.Processor(stop)
     normalized_corpus, lines = pr.process(corpus)
     del lines
-    print normalized_corpus[:10]
-    tfidf = Tfidf(ngram_range=(1,2), binary=True, tokenizer=tweet_tokenizer)
+    tfidf = Tfidf(ngram_range=(1,2), binary=True,
+                  tokenizer=tweet_tokenizer)
     X = tfidf.fit_transform(normalized_corpus)
+    return (X, labels)
+
+def eval_params(X, labels, C, gamma):
     cutoff = int(X.shape[0]*.7)
     svm = SVC(C=C, gamma=gamma)
     X_train, y_train = X[:cutoff], labels[:cutoff]
@@ -26,7 +29,11 @@ def main(C, gamma):
     svm.fit(X_train, y_train)
     preds = svm.predict(X_test)
     fitness = (preds == y_test).sum() / (y_test.shape[0]+.0)
-    print fitness
+    return fitness
+
+def main(C, gamma):
+    X, labels = process_corpus()
+    print eval_params(X, labels, C, gamma)
 
 if __name__ == '__main__':
     C = float(sys.argv[1])
