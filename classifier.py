077a228a362f90d88b890cd8f47a7d99650750af
Guilherme Peixoto <gpp@cin.ufpe.br>
2016-06-25 13:18:56 -0300

    main method and sort fix

--- classifier.py
+++ classifier.py
@@ -7,35 +7,39 @@ from twokenize import tokenize as tweet_tokenizer
 import Processor, sys
 import numpy as np
 
-def process_corpus():
-    lines = [eval(line.strip()) for line in open("tweetlabels.txt", "r")]
-    stop = stopwords.words("english")
-    stop.remove("no")
-    corpus = [x[0] for x in lines]
-    labels = [x[1] for x in lines]
-    pr = Processor.Processor(stop)
-    normalized_corpus, lines = pr.process(corpus)
-    del lines
-    tfidf = Tfidf(ngram_range=(1,2), binary=True,
-                  tokenizer=tweet_tokenizer)
-    X = tfidf.fit_transform(normalized_corpus)
-    return (X, labels)
+class ML:
+    def __init__(self):
+        self.X = None
+        self.labels = None
 
-def eval_params(X, labels, C, gamma):
-    cutoff = int(X.shape[0]*.7)
-    svm = SVC(C=C, gamma=gamma)
-    X_train, y_train = X[:cutoff], labels[:cutoff]
-    X_test, y_test = X[cutoff:], np.array(labels[cutoff:])
-    svm.fit(X_train, y_train)
-    preds = svm.predict(X_test)
-    fitness = (preds == y_test).sum() / (y_test.shape[0]+.0)
-    return fitness
+    def process_corpus(self):
+        lines = [eval(line.strip()) for line in open("tweetlabels.txt", "r")]
+        stop = stopwords.words("english")
+        stop.remove("no")
+        corpus = [x[0] for x in lines]
+        labels = [x[1] for x in lines]
+        pr = Processor.Processor(stop)
+        normalized_corpus, lines = pr.process(corpus)
+        del lines
+        tfidf = Tfidf(ngram_range=(1,2), binary=True,
+                      tokenizer=tweet_tokenizer)
+        X = tfidf.fit_transform(normalized_corpus)
+        self.X = X
+        self.labels = labels
 
-def main(C, gamma):
-    X, labels = process_corpus()
-    print eval_params(X, labels, C, gamma)
-
-if __name__ == '__main__':
-    C = float(sys.argv[1])
-    gamma = float(sys.argv[2])
-    main(C, gamma)
+    def eval_params(self, nparr):
+        cutoff = int(self.X.shape[0]*.7)
+        C = nparr[0]
+        gamma = nparr[1]
+        if gamma < 0:
+            gamma = 1e-3
+        if C < 0:
+            C = 1e-3
+        svm = SVC(C=C, gamma=gamma)
+        X_train, y_train = self.X[:cutoff], self.labels[:cutoff]
+        X_test, y_test = self.X[cutoff:], np.array(self.labels[cutoff:])
+        svm.fit(X_train, y_train)
+        preds = svm.predict(X_test)
+        fitness = (preds == y_test).sum() / (y_test.shape[0]+.0)
+        print "fitness: %.6f achieved with C=%.6f and gamma=%.6f" % (fitness, C, gamma)
+        return fitness
